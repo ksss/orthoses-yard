619b4ab1c1e576066f2ca88c4891506331975907
ksss <co000ri@gmail.com>
2022-10-10 17:43:24 +0900

    Split file to templates

--- examples/yard/generate.rb
+++ examples/yard/generate.rb
@@ -1,6 +1,7 @@
 require 'orthoses-yard'
 require 'fileutils'
 require 'pathname'
+require 'erb'
 
 FileUtils.rm_rf('out')
 Orthoses.logger.level = :warn
@@ -40,99 +41,32 @@ Orthoses::Builder.new do
     YARD::Tags::Library.define_tag("YARD Tag Signature", 'yard.signature'.to_sym, nil)
     YARD::Tags::Library.define_tag("YARD Tag", 'yard.tag'.to_sym, :with_types_and_name)
     YARD::Tags::Library.define_tag("YARD Directive", 'yard.directive'.to_sym, :with_types_and_name)
-    # YARD::Tags::Library.visible_tags -= ['yard.tag'].map(&:to_sym)
   }
 end.call
 
+stdlib_dependencies = %w[
+  set
+  optparse
+  logger
+  monitor
+]
+
+def erb(template_filename, **vars)
+  "templates/#{template_filename}"
+    .then { File.expand_path(_1) }
+    .then { File.read(_1) }
+    .then { ERB.new(_1, trim_mode: '<>').result_with_hash(vars) }
+end
+
 out = Pathname("out")
-out.join("EXTERNAL_TODO.rbs").write(<<RBS)
-  # !!! GENERATED CODE !!!
-  class OpenStruct
-  end
-
-  class Ripper
-  end
-
-  module Rake
-    class TaskLib
-    end
-  end
-
-
-  module WEBrick
-    module HTTPServlet
-      class AbstractServlet
-      end
-    end
-  end
-
-  module RDoc
-    module Markup
-      class ToHtml
-      end
-    end
-  end
-RBS
-
-out.join("manifest.yaml").write(<<~YAML)
-dependencies:
-  - name: rubygems
-  - name: set
-  - name: optparse
-  - name: logger
-  - name: monitor
-YAML
-
-out.join('_scripts').tap(&:mkpath).join("test").write(<<~SHELL)
-#!/usr/bin/env bash
-
-# Exit command with non-zero status code, Output logs of every command executed, Treat unset variables as an error when substituting.
-set -eou pipefail
-# Internal Field Separator - Linux shell variable
-IFS=$'
-	'
-# Print shell input lines
-set -v
-
-# Set RBS_DIR variable to change directory to execute type checks using `steep check`
-RBS_DIR=$(cd $(dirname $0)/..; pwd)
-# Set REPO_DIR variable to validate RBS files added to the corresponding folder
-REPO_DIR=$(cd $(dirname $0)/../../..; pwd)
-# Validate RBS files, using the bundler environment present
-bundle exec rbs --repo $REPO_DIR -r yard:0.9 -r rubygems -r set -r optparse -r logger -r monitor -r rack:2.2.2 validate --silent
-
-cd ${RBS_DIR}/_test
-# Run type checks
-bundle exec steep check
-SHELL
-
-out.join('_test').tap(&:mkpath).join("yard.rb").write(<<~RUBY)
-require 'yard'
-
-YARD::Registry.root.children.each do |child|
-  child.name(false)
-  child.docstring.all
+out.join("EXTERNAL_TODO.rbs").write(erb("EXTERNAL_TODO.rbs"))
+out.join("manifest.yaml").write(erb("manifest.yaml.erb", stdlib_dependencies: stdlib_dependencies))
+out.join('_scripts').tap do |scripts|
+  scripts.mkpath
+  scripts.join("test").write(erb("_scripts/test.erb", stdlib_dependencies: stdlib_dependencies))
 end
-RUBY
-
-out.join('_test').join('Steepfile').write(<<~RUBY)
-D = Steep::Diagnostic
-
-target :test do
-  check "."
-  signature '.'
-
-  repo_path "../../../"
-
-  library "rubygems"
-  library "set"
-  library "optparse"
-  library "logger"
-  library "monitor"
-
-  library "yard:0.9"
-  library "rack:2.2.2"
-
-  configure_code_diagnostics(D::Ruby.all_error)
+out.join('_test').tap do |test|
+  test.mkpath
+  test.join("yard.rb").write(erb("_test/yard.rb"))
+  test.join('Steepfile').write(erb("_test/Steepfile.erb", stdlib_dependencies: stdlib_dependencies))
 end
-RUBY
